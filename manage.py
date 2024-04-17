bedd3be4a04cd9e89de437c4fb6f3a24799b53c8
Junaid Saleem <junaidsaleem986@gmail.com>
2024-04-18 00:02:39 +0500

    Adding Files

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbms.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbrs.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
