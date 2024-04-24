464bed990681a7faae27a0b66c13ae7f95eb287d
Junaid Saleem <junaidsaleem986@gmail.com>
2024-04-24 08:33:26 +0500

    Update models.py

--- system/models.py
+++ system/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+    
 
 Destination_Choices = (
     ("GIKI","GIKI"),
@@ -16,17 +18,15 @@ class destination(models.Model):
     bus_No = models.IntegerField()
     date = models.DateTimeField(auto_now=False, auto_now_add=False)
     bus_location = models.CharField(max_length=100)
+    tickets = models.IntegerField(default=30)
     def __str__(self) -> str:
         return self.from_destination+ "-" + self.to_destination
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=10)
-    password = models.CharField(max_length=10)
+class ticket(models.Model):
+    user_pk = models.ForeignKey(User, on_delete=models.CASCADE)
+    dest_pk = models.ForeignKey(destination, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return self.username
-
-
+    def __str__(self):
+        return f"Ticket of {self.user_pk.username} for {self.dest_pk}"
 
 
