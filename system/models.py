1524408037d356f5f06ab6e9e63f6723d3d7d028
Junaid Saleem <junaidsaleem986@gmail.com>
2024-05-08 00:00:15 +0500

    Update models.py

--- system/models.py
+++ system/models.py
@@ -9,16 +9,25 @@ Destination_Choices = (
     ("Peshawar","Peshawar"),
     ("Abbotabad","Abbotabad"),
     ("Pindi","Pindi"),
-    ("Islamabad","Islamabad")
+    ("Islamabad","Islamabad"),
+    ("Lahore","Lahore")
 )
+class driver(models.Model):
+    driver_name = models.CharField(max_length=30)
+    driver_number = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.driver_name
 
 class destination(models.Model):
     from_destination = models.CharField(max_length=20,choices=Destination_Choices)
     to_destination = models.CharField(max_length=20,choices=Destination_Choices)
     bus_No = models.IntegerField()
-    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    date =models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False) 
     bus_location = models.CharField(max_length=100)
     tickets = models.IntegerField(default=30)
+    bus_driver = models.ForeignKey(driver,on_delete=models.CASCADE)
     def __str__(self) -> str:
         return self.from_destination+ "-" + self.to_destination
 
